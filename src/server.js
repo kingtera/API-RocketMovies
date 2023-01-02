777a091a58172ffc69bc3e403ea05c174b4e1134
eduardo <eduardoteranisi@gmail.com>
2023-01-02 12:26:11 -0300

    add error treatment with 'AppError.js'

--- src/server.js
+++ src/server.js
@@ -1,11 +1,25 @@
+const AppError = require("./utils/AppError")
 const express = require("express")
-
 const routes = require("./routes") //por padrão, vai rodar o index.js
-
 const app = express() //inicializa o express
+
 app.use(express.json())
 
 app.use(routes)
+
+app.use((error, request, response, next) => {
+    if(error instanceof AppError) {
+        return response.status(error.statusCode).json({
+            status: "error",
+            message: error.message 
+        })
+    }
+
+    return response.status(500).json({
+        status: "error",
+        message: "Server Error"
+    })
+})
 
 const PORT = 3333
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
