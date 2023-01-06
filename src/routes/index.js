03b2c6990450dbe29c4e168f92b1fd4f94d29a8d
eduardo <eduardoteranisi@gmail.com>
2023-01-06 13:19:23 -0300

    create class NotesController and one method create

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,13 @@
 const { Router } = require("express")
 
 const usersRoutes = require("./users.routes")
+const notesRoutes = require("./movie_notes.routes")
+// const tagsRoutes = require("./movie_tags.routes")
 
 const routes = Router()
 
 routes.use("/users", usersRoutes)
+routes.use("/movie_notes", notesRoutes)
+// routes.use("/movie_tags", tagsRoutes)
 
 module.exports = routes 
