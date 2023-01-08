a5e5a23cd99e5b8ebfffb38d5da027ff21538df8
eduardo <eduardoteranisi@gmail.com>
2023-01-08 14:28:57 -0300

    create function 'index' in 'TagsController'

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -47,12 +47,41 @@ class NotesController{
     }
 
     async index(request, response) {
-        const { user_id } = request.query
+        const { title, user_id, tags} = request.query
 
-        const notes = await knex("movie_notes")
-        .where({ user_id })
+        let notes
 
-        response.json({notes})
+        if(tags) {
+            const filterTags = tags.split(',').map(tag => tag.trim())
+
+            notes = await knex("movie_tags")
+            .select([
+                "movie_notes.id",
+                "movie_notes.title",
+                "movie_notes.user_id"
+            ])
+                .where("movie_notes.user_id", user_id)
+                .whereLike("movie_notes.title", `%${title}%`)
+                .whereIn("name", filterTags)
+                .innerJoin("movie_notes", "movie_notes.id", "movie_tags.note_id")
+                .orderBy("movie_notes.title")
+        }else{
+            notes = await knex("movie_notes")
+            .where({ user_id })
+            .whereLike("title", `%${title}%`)
+        }
+
+        const userTags = await knex("movie_tags").where({ user_id })
+        const notesWithTags = notes.map(note => {
+            const noteTags = userTags.filter(tag => tag.note_id === note.id)
+            
+            return {
+                ...note,
+                tags: noteTags
+            }
+        })
+
+        response.json(notesWithTags)
     }
 }
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,12 @@ const { Router } = require("express")
 
 const usersRoutes = require("./users.routes")
 const notesRoutes = require("./movie_notes.routes")
-// const tagsRoutes = require("./movie_tags.routes")
+const tagsRoutes = require("./movie_tags.routes")
 
 const routes = Router()
 
 routes.use("/users", usersRoutes)
 routes.use("/movie_notes", notesRoutes)
-// routes.use("/movie_tags", tagsRoutes)
+routes.use("/movie_tags", tagsRoutes)
 
 module.exports = routes 
